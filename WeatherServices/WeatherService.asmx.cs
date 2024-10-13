0a3f98d5dfc3be586924970b8640e337342bc59d
Muthupandi Chandran <pp@DESKTOP-S1M0R1Q>
2024-10-13 13:34:42 +0530

    Add project files.

---
+++ WeatherServices/WeatherService.asmx.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Services;
+
+namespace WeatherServices
+{
+    /// <summary>
+    /// Summary description for WeatherService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class WeatherService : System.Web.Services.WebService
+    {
+        private static readonly string apiKey = "611cf6224ebc8cafd76070bcbb2c2f83"; //GET API from Open Weather website Profile
+
+        [WebMethod]
+        public string GetWeatherByCity(string city)
+        {
+            try
+            {
+                string apiUrl = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+
+                // Create a WebRequest and use GetResponse() to retrieve weather data
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiUrl);
+                request.Method = "GET";
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string jsonData = reader.ReadToEnd();
+                        var weatherData = JObject.Parse(jsonData);
+
+                        string weatherDescription = weatherData["weather"][0]["description"].ToString();
+                        string temperature = weatherData["main"]["temp"].ToString();
+
+                        return $"Weather in {city}: {weatherDescription}, Temperature: {temperature}°C";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Error: {ex.Message}";
+            }
+        }
+    }
+}
---
+++ WeatherServices/WebForm1.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WeatherServices.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager ID="ScriptManager1" runat="server">
+    <Services>
+        <asp:ServiceReference Path="~/WeatherService.asmx" />
+    </Services>
+</asp:ScriptManager>
+       <table style="font-family:Arial;border:1px solid black">
+<tbody>
+<tr>
+<td><b>City</b></td>
+<td><input name="txtCity" type="text" id="txtCity"/>
+<input id="Button" type="button" value="Get Weather Details" onclick="GetWeatherDetails()" />
+</td>
+</tr>
+<tr>
+<td><b>Weather Description:</b></td>
+<td><input name="txtDescription" type="text" id="txtDescription" /></td>
+</tr>
+<tr>
+<td><b>Temprature(°C):</b></td>
+<td><input name="txtWeather" type="text" id="txtWeather"/></td>
+</tr>
+</tbody>
+</table>
+    </form>
+</body>
+</html>
+
+<script type="text/javascript">
+    function GetWeatherDetails()
+    {
+        var city = document.getElementById("txtCity").value;
+        WeatherServices.WeatherService.GetWeatherByCity(city,GetWeatherByCitySuccessCallback,GetWeatherByCityFailedCallback);
+    }
+    function GetWeatherByCitySuccessCallback(results)
+    {
+        /*return $"Weather in {city}: {weatherDescription}, Temperature: {temperature}°C";*/
+        var weatherinfo = results.split(",");
+        var weatherDescription = weatherinfo[0].split(":")[1].trim();
+        var temprature = weatherinfo[1].split(":")[1].trim();
+        document.getElementById("txtDescription").value = weatherDescription;
+        document.getElementById("txtWeather").value = temprature;
+    }
+    function GetWeatherByCityFailedCallback(error) {
+        alert("Error fetching weather details: " + error._message); // Show an error message if the request fails
+    }
+
+
+</script>
